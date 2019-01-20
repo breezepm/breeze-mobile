6fc77e55f8c8964b40ee07b7d2385d688c36bed0
Rene Vahtel <rene.vahtel@gmail.com>
2019-01-20 10:44:04 +0200

    fix autocorrect

--- src/app/modules/tasks/modals/task-status/task-status.modal.ts
+++ src/app/modules/tasks/modals/task-status/task-status.modal.ts
@@ -1,32 +1,34 @@
-import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { ViewController } from 'ionic-angular';
-import { Subject } from 'rxjs/';
+import { Component } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { ViewController } from "ionic-angular";
+import { Subject } from "rxjs/";
 
-import { AppState } from './../../../../app.reducer';
-import { updateStatus } from './../../actions/task.actions';
-import { Task, TaskStatuses } from './../../../../models/tasks/task.model';
-import { isDefined } from './../../../../helpers/path-is-defined';
-import { clone } from 'ramda';
+import { AppState } from "./../../../../app.reducer";
+import { updateStatus } from "./../../actions/task.actions";
+import { Task, TaskStatuses } from "./../../../../models/tasks/task.model";
+import { isDefined } from "./../../../../helpers/path-is-defined";
+import { clone } from "ramda";
 
 @Component({
-  selector: 'modal-task-status',
-  templateUrl: 'task-status.modal.html',
-  styles: [ 'task-status.modal.scss' ],
+  selector: "modal-task-status",
+  templateUrl: "task-status.modal.html",
+  styles: ["task-status.modal.scss"]
 })
 export class TaskStatusModal {
-
   public selectedStatus: TaskStatuses = {
     id: null,
     color: null,
-    name: null,
+    name: null
   };
 
   private killer$: Subject<any> = new Subject();
 
   private task: Task;
 
-  constructor(private store: Store<AppState>, private viewCtrl: ViewController) {}
+  constructor(
+    private store: Store<AppState>,
+    private viewCtrl: ViewController
+  ) {}
 
   public ionViewWillLoad(): void {
     this.getTask();
@@ -42,14 +44,23 @@ export class TaskStatusModal {
   }
 
   public selectStatusOnList(status: any) {
-    this.selectedStatus = status;
+    if (status.id == this.selectedStatus.id) {
+      this.selectedStatus = {
+        id: null,
+        color: null,
+        name: null
+      };
+    } else {
+      this.selectedStatus = status;
+    }
   }
 
   private getTask(): void {
-    this.store.select<Task>('task', 'fetchTask', 'value')
+    this.store
+      .select<Task>("task", "fetchTask", "value")
       .takeUntil(this.killer$)
       .filter(isDefined)
-      .subscribe((task) => {
+      .subscribe(task => {
         this.task = clone(task);
         this.selectedStatus.id = task.status_id;
       });
@@ -57,18 +68,20 @@ export class TaskStatusModal {
 
   private updateTaskStatus(): void {
     if (this.statusIdChanged) {
-      this.store.dispatch(updateStatus({
-        ...this.task,
-        status_id: this.selectedStatus.id,
-        status_color: this.selectedStatus.color,
-        status_name: this.selectedStatus.name,
-        status_short: null,
-        status_code: null,
-        done: null,
-        ready: null,
-        onhold: null,
-        blocked: null,
-      }));
+      this.store.dispatch(
+        updateStatus({
+          ...this.task,
+          status_id: this.selectedStatus.id,
+          status_color: this.selectedStatus.color,
+          status_name: this.selectedStatus.name,
+          status_short: null,
+          status_code: null,
+          done: null,
+          ready: null,
+          onhold: null,
+          blocked: null
+        })
+      );
     }
   }
 
